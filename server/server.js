9ad78141614aa2eea291cadec2cae8d60a674f76
CodeCraft <Bmosquera0510@gmail.com>
2024-11-28 11:56:24 -0500

    Cambio de políticas CORS

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const projectRoutes = require("./routes/project.route.js");
 
 const port = process.env.PORT || 3000; 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://todo-frontend-ks86.onrender.com/",
   credentials: true,
 };
 
